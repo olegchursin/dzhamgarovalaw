e00fedf16379de213e4393dfd6bfdbbd3c6ba42f
olegchursin <olegchursin@gmail.com>
2017-05-09 17:08:45 -0400

    major content update

--- js/main.js
+++ js/main.js
@@ -1,4 +1,8 @@
 $( document ).ready(function(){
-  /*Initialize mobile side nav*/
+  // Initialize mobile side nav
   $(".button-collapse").sideNav();
+
+  // Modal trigger
+  // the "href" attribute of .modal-trigger must specify the modal ID that wants to be triggered
+    $('.modal').modal();
 })
