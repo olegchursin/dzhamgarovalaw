b328d0b594704eb4655f005ec4a9ca677849bce0
olegchursin <olegchursin@gmail.com>
2017-12-28 15:19:31 -0500

    add RU versions of individual pages

--- js/main.js
+++ js/main.js
@@ -1,8 +1,13 @@
-$( document ).ready(function(){
-  // Initialize mobile side nav
-  $(".button-collapse").sideNav();
+$(document).ready(function() {
+    // Initialize mobile side nav
+    $(".button-collapse").sideNav();
 
-  // Modal trigger
-  // the "href" attribute of .modal-trigger must specify the modal ID that wants to be triggered
+    // Modal trigger
+    // the "href" attribute of .modal-trigger must specify the modal ID that wants to be triggered
     $('.modal').modal();
+
+    // Lang selector trigger
+    $(".lang-selector").click(function() {
+        $(this).toggleClass("show");
+    });
 })
